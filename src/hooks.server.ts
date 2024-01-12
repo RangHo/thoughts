b7d2a3ea5c42b8153b9637b777a8a0b05f51ecfe
RangHo Lee <hello@rangho.me>
2024-01-13 00:26:54 +0100

    feat: set document language dynamically

---
+++ src/hooks.server.ts
@@ -0,0 +1,20 @@
+import type { Handle } from '@sveltejs/kit';
+
+import { getMetadata } from '$lib/post';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const slug = event.params.slug || '';
+
+  let language: string;
+  if (slug === '') {
+    language = 'ko';
+  } else {
+    language = (await getMetadata(slug)).language;
+  }
+  
+  return await resolve(event, {
+    transformPageChunk({ html }) {
+      return html.replace('%lang%', language);
+    }
+  });
+};

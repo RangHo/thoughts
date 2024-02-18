c0f24175e66096e931e3fe39884baf2f9118e413
RangHo Lee <hello@rangho.me>
2024-02-18 22:39:01 +0100

    feat: import assets automatically

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    importAssets()
+  ],
 
   kit: {
     // Use static adapter to render individual pages as static HTML

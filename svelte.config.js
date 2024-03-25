03d83d9f693e11c1cf23cfaf97d37e27a052a404
RangHo Lee <hello@rangho.me>
2024-03-25 11:05:07 +0900

    chore: add melt-ui as dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { importAssets } from 'svelte-preprocess-import-assets';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import orgPreprocess from 'svelte-preprocess-org';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-  preprocess: [
+  preprocess: sequence([
     orgPreprocess({
       extensions: ['.org'],
       latexEnvironmentFormat: '<Math expression={%s} display />',
@@ -18,27 +20,23 @@ const config = {
     }),
     vitePreprocess(),
     importAssets(),
-  ],
-
+    preprocessMeltUI(),
+  ]),
   extensions: ['.svelte', '.org'],
-
   kit: {
     // Use static adapter to render individual pages as static HTML
     adapter: adapter({
       // GitHub Pages requires a custom 404.html
       fallback: '404.html',
     }),
-
     // Prerendering settings
     prerender: {
       handleHttpError: 'warn',
     },
-
     // Declare custom import aliases
     alias: {
       $posts: './src/posts',
     },
   },
 };
-
 export default config;
